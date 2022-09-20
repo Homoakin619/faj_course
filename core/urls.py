a8e29fc37f3944c2061461f9049611ce4e699860
Homoakin619 <homohaykins619@gmail.com>
2022-09-20 16:57:01 +0100

    Initial commit, added deployment configurations

---
+++ core/urls.py
@@ -0,0 +1,16 @@
+from core import views
+from django.urls import path
+
+urlpatterns = [
+    path('',views.HomepageView.as_view(),name='home'),
+    path('login/',views.LoginRegisterView.as_view(),name='login'),
+    path('create/',views.AdminAddPostView.as_view(),name='create_post'),
+    path('edit/<int:pk>',views.AdminEditPostView.as_view(),name='edit_post'),
+    path('dashboard/',views.AdminDashboardView.as_view(),name='dashboard'),
+    path('all_posts/',views.AdminAllPosts.as_view(),name='all_posts'),
+    path('user/<int:pk>/',views.AdminEditUserView.as_view(),name='edit_user'),
+    path('logout/',views.logout_user,name='logout'),
+    path('not_verified/',views.RedirectPage.as_view(),name='not_verified'),
+    path('contents/<int:pk>/',views.PostDetail.as_view(),name='post_detail'),
+
+]
