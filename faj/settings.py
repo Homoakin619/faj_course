8dc018f6d7d29307a10731a86397e9856a496451
Homoakin619 <homohaykins619@gmail.com>
2022-09-21 14:41:02 +0100

    removed email backend settings

--- faj/settings.py
+++ faj/settings.py
@@ -152,12 +152,12 @@ LOGGING = {
 }
 
 if not DEBUG:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = env('EMAIL_HOST')
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
+    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    # EMAIL_HOST = env('EMAIL_HOST')
+    # EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    # EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    # EMAIL_PORT = 587
+    # EMAIL_USE_TLS = True
 
     CLOUDINARY_STORAGE = {
         'CLOUD_NAME': env('CLOUD_NAME'),
